a290533ecb99f2d29b049559ae408b1e56896d1b
Augustin Chan <aug@iterativeinsights.io>
2023-11-04 02:08:43 +0800

    Iterative Insights site, initial version

---
+++ theme-toggle.js
@@ -0,0 +1,10 @@
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    var body = document.body;
+    if (body.classList.contains('light-theme')) {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+    } else {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+    }
+});
