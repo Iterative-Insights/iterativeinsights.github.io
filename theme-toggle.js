87c9d680bfe5347d2350e149ee5646b9e976f93d
Augustin Chan <aug@iterativeinsights.io>
2023-11-05 08:11:40 +0800

    Add transparent spirals, incorporate to toggle

--- theme-toggle.js
+++ theme-toggle.js
@@ -1,10 +1,18 @@
 document.getElementById('theme-toggle').addEventListener('click', function() {
     var body = document.body;
+    var imageDiv = document.querySelector('.poster .spiral-image');
+
     if (body.classList.contains('light-theme')) {
         body.classList.remove('light-theme');
         body.classList.add('dark-theme');
+
+        imageDiv.classList.remove('spiral-image-light-theme');
+        imageDiv.classList.add('spiral-image-dark-theme');
     } else {
         body.classList.remove('dark-theme');
         body.classList.add('light-theme');
+
+        imageDiv.classList.remove('spiral-image-dark-theme');
+        imageDiv.classList.add('spiral-image-light-theme');
     }
 });
