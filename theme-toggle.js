bab771123fe634a016699c0d30e40bb129dc4c7d
Augustin Chan <aug@iterative.day>
2024-08-09 13:04:51 +0800

    change default to dark

--- theme-toggle.js
+++ theme-toggle.js
@@ -1,18 +1,22 @@
-document.getElementById('theme-toggle').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function() {
     var body = document.body;
     var imageDiv = document.querySelector('.poster .spiral-image');
+    
+    // Set initial state
+    body.classList.add('dark-theme');
+    imageDiv.classList.add('spiral-image-dark-theme');
 
-    if (body.classList.contains('light-theme')) {
-        body.classList.remove('light-theme');
-        body.classList.add('dark-theme');
-
-        imageDiv.classList.remove('spiral-image-light-theme');
-        imageDiv.classList.add('spiral-image-dark-theme');
-    } else {
-        body.classList.remove('dark-theme');
-        body.classList.add('light-theme');
-
-        imageDiv.classList.remove('spiral-image-dark-theme');
-        imageDiv.classList.add('spiral-image-light-theme');
-    }
+    document.getElementById('theme-toggle').addEventListener('click', function() {
+        if (body.classList.contains('dark-theme')) {
+            body.classList.remove('dark-theme');
+            body.classList.add('light-theme');
+            imageDiv.classList.remove('spiral-image-dark-theme');
+            imageDiv.classList.add('spiral-image-light-theme');
+        } else {
+            body.classList.remove('light-theme');
+            body.classList.add('dark-theme');
+            imageDiv.classList.remove('spiral-image-light-theme');
+            imageDiv.classList.add('spiral-image-dark-theme');
+        }
+    });
 });
